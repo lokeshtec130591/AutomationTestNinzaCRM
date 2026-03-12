f71a522193068b1b5d62ad872716d2186bce5a20
Lokesh-PC <Lokesh-PC@Lokesh>
2026-03-12 07:51:52 +0530

    First Commit

---
+++ src/test/java/Practice/demo1Test.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class demo1Test {
+
+	
+	@Test
+	public void carTest() {
+		
+		System.out.println("BMW");
+	}
+	
+}
---
+++ src/test/java/Practice/demoTest.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+
+	@Test(groups = "smoke")
+	public void bikeTest() {
+		
+		System.out.println("Royal Enfield");
+	}
+	
+	
+	
+}
